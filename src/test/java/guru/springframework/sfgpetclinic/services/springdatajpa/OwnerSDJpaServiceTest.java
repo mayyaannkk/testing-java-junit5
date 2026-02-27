537554d6096ad634bb5cac4b010e6eb8161ce88f
mayyaannkk <mayankpant.dev@gmail.com>
2026-02-27 19:28:40 +0530

    skipping tests

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled(value = "Disabled until we learn Mocking")
 class OwnerSDJpaServiceTest {
 
     OwnerSDJpaService ownerSDJpaService;
@@ -14,8 +17,10 @@ class OwnerSDJpaServiceTest {
         ownerSDJpaService = new OwnerSDJpaService(null, null, null);
     }
 
+    @Disabled
     @Test
     void findByLastName() {
+        Owner owner = ownerSDJpaService.findByLastName("Buck");
     }
 
     @Test
