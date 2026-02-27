a4a8300935b16c8aedf9644b594c8b689908cfa0
mayyaannkk <mayankpant.dev@gmail.com>
2026-02-27 18:01:30 +0530

    added assertEquals and assertTrue basic ones

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -7,26 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class IndexControllerTest {
 
-    IndexController controller;
+    IndexController indexController;
 
     @BeforeEach
     void setUp() {
-        controller = new IndexController();
+        indexController = new IndexController();
     }
 
     @Test
     void index() {
-        assertEquals("index", controller.index());
-        assertEquals("index", controller.index(), "Wrong View Returned");
-
-        assertEquals("index", controller.index(), () -> "Another Expensive Message " +
-                "Make me only if you have to");
+        assertEquals("index", indexController.index());
+        assertEquals("index", indexController.index(), "Message shown when test fails");
+        assertEquals("index", indexController.index(), () -> "Overloaded assertEquals method that takes in a lambda to generate message. But the message is only generated when it is needed, i.e. at failure.");
     }
 
     @Test
     void oupsHandler() {
-        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive " +
-                "Message to build" +
-                "for my test");
+        assertTrue("notimplemented".equals(indexController.oupsHandler()), () -> "This lambda is used when some expensive string message is to be built for my test");
     }
 }
